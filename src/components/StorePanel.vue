3c9bb8e06b183e12b684bc8c95c5a07b6545a9e0
Elman <xzample101@gmail.com>
2022-10-15 01:54:01 +0400

    add: user panel and store panel components

---
+++ src/components/StorePanel.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="store-panel">
+    <div class="block">
+      <div class="store-panel__picked-item" :class="{ item: storePickedItem }">
+        {{ storePickedItem ? storePickedItem.name : "Store picked item" }}
+      </div>
+    </div>
+    <div class="block">
+      <div class="store-panel__items items">
+        <div
+          v-for="item in storeItems"
+          :key="item.id"
+          class="store-panel__item item"
+          :class="{ 'item--active': isPicked(item) }"
+          @click="clickOnItem(item)"
+        >
+          {{ item.name }}
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "StorePanel",
+  props: {
+    storePickedItem: {
+      type: Object,
+      default() {
+        return {};
+      },
+    },
+    storeItems: {
+      type: Array,
+      default() {
+        return [];
+      },
+    },
+  },
+  methods: {
+    isPicked(item) {
+      return this.storePickedItem?.id === item.id;
+    },
+    clickOnItem(item) {
+      this.$emit("click-on-item", item);
+    },
+  },
+};
+</script>
+
+<style scoped></style>
